0bdd6e8be9c77abf0bbe2bed40771feda8e28f37
newphew92 <terrenceko@outlook.com>
2018-04-24 18:21:04 -0400

    Create kubSetup.sh

---
+++ kubSetup.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+apt-get install curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+
+wget https://yum.dockerproject.org/gpg
+apt-key add gpg
+
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubectl
