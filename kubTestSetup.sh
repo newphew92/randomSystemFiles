7a199f47437e5f71e830bd55f1fe80241c4a5937
newphew92 <terrenceko@outlook.com>
2018-04-25 17:55:35 -0400

    Create kubTestSetup.sh

---
+++ kubTestSetup.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#if you didn't install it already
+
+#sudo apt-get update
+#sudo apt-get install nginx
+
+#don't need to run this if you just installed nginx
+
+#systemctl start nginx
